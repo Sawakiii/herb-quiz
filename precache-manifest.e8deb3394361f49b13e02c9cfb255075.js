a6e7ed175fcdff8d74dbfd73651ac949028db09c
Sawaki <you@example.com>
2020-06-09 11:33:09 +0900

    Updates

---
+++ precache-manifest.e8deb3394361f49b13e02c9cfb255075.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d308e6faf03a1ee9dd15cb9cae4a910d",
+    "url": "/herb-quiz/index.html"
+  },
+  {
+    "revision": "5288ed89cdcb6dc5e423",
+    "url": "/herb-quiz/static/css/main.7a73bef7.chunk.css"
+  },
+  {
+    "revision": "e51715b49c923b3246c6",
+    "url": "/herb-quiz/static/js/2.70ce6bbe.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/herb-quiz/static/js/2.70ce6bbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5288ed89cdcb6dc5e423",
+    "url": "/herb-quiz/static/js/main.1bae7ab4.chunk.js"
+  },
+  {
+    "revision": "812ec5568c2eee4d5cbf",
+    "url": "/herb-quiz/static/js/runtime-main.cbee3294.js"
+  }
+]);
